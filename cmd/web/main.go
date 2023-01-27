221aa7ccb7bfd23ce5187a3c1c6a182b23db7779
IfedayoAwe <olalekanawe99@gmail.com>
2023-01-27 04:03:29 +0100

    errorfiles

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -16,16 +18,31 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	var err error
+	mydir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	infoFile, err := os.OpenFile(filepath.Join(mydir, "/info.log"), os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer infoFile.Close()
+	infoLog := log.New(infoFile, "INFO\t", log.Ldate|log.Ltime)
+	errFile, err := os.OpenFile(filepath.Join(mydir, "/error.log"), os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer errFile.Close()
+	errorLog := log.New(errFile, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
 		Handler:  mux,
 	}
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
