37ea132e70d27f54261c869b3819a5afe4bbeb0c
IfedayoAwe <olalekanawe99@gmail.com>
2023-01-27 02:41:02 +0100

    adding custom logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -18,7 +18,7 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
 	infoLog.Printf("Starting server on %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
 	errorLog.Fatal(err)
