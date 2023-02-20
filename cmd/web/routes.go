acbde3e334c939de416d8d52aeec7f06cd07015d
IfedayoAwe <olalekanawe99@gmail.com>
2023-02-20 14:06:33 +0100

    change password

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,6 +23,8 @@ func (app *application) routes() http.Handler {
 	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
 	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
 	mux.Post("/user/logout", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.logoutUser))
+	mux.Get("/user/change-password", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.changePasswordForm))
+	mux.Post("/user/change-password", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.changePassword))
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 	mux.Get("/snippet/download/", http.StripPrefix("/snippet/download", http.HandlerFunc(app.downloadHandler)))
