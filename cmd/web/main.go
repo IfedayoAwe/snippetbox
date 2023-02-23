f32b91ea985c3585a2bc730b3ebfbaabe0301bb9
IfedayoAwe <olalekanawe99@gmail.com>
2023-02-23 17:27:07 +0100

    source

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -19,6 +19,8 @@ import (
 	"github.com/golangcollege/sessions"
 )
 
+var version string // do not remove or modify
+
 type contextKey string
 
 const contextKeyIsAuthenticated = contextKey("isAuthenticated")
